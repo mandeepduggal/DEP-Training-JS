a822bf3a036c3b64d84da78958166439eba67d3b
Vishal <32020283+vishal8885@users.noreply.github.com>
2020-04-16 12:22:33 +0530

    Revert "Node task1"

--- Angular/Assign3/src/app/full-name.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FullNamePipe } from './full-name.pipe';
-
-xdescribe('FullNamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FullNamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
